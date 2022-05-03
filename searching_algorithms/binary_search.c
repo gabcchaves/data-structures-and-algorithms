b1b54754fadf7a8a4667f1b0c4ca963de741d897
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-05-03 16:16:20 -0300

    Binary search algorithm.

---
+++ searching_algorithms/binary_search.c
@@ -0,0 +1,54 @@
+// Binary searching algorithm
+#include <stdio.h>
+
+// Implementation
+int binarySearch( int * array, int arrayLength, int element ) {
+	// Lower and Upper bounds
+	int LBound = array[0], UBound = array[arrayLength-1];
+
+	while ( LBound <= UBound ) {
+		// The mid point of the current sub-array
+		int midPoint = (LBound + UBound) / 2;
+		
+		// Element is in the mid point?
+		if ( element == array[midPoint] ) return midPoint;
+
+		// Element is on the left?
+		if ( element < array[midPoint] ) {
+			UBound = midPoint;
+			continue;
+		}
+		// Element is on the right?
+		if ( element > array[midPoint] ) {
+			LBound = midPoint;
+			continue;
+		}	
+	}
+	return -1;
+}
+
+// Print the array
+void printArray( int * , int );
+
+void main() {	
+	int array[] = {1, 32, 43, 12, 33, 5, 8};
+	int element = 33;
+	size_t arrayLength = sizeof(array) / sizeof(array[0]);
+
+	int index = binarySearch( array, arrayLength, element );
+	
+	if ( index != -1 ) {
+		printf("%d está na posição %d.\n", element, index);
+		printArray( array, arrayLength );
+	} else {
+		printf("Elemento não encontrado.\n");
+	}
+}
+
+void printArray( int * array, int arrayLength ) {
+	printf("[ ");
+	for ( int i = 0; i < arrayLength; i++ ) {
+		printf("%d ", array[i]);
+	}
+	printf("]\n");
+}
