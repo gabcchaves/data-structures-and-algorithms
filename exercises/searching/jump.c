04218ab6d52bcc3cf903abcdf9e302134c7ef878
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-05-30 19:47:35 -0300

    I had to fix the code.

--- exercises/searching/jump.c
+++ exercises/searching/jump.c
@@ -4,16 +4,20 @@
 
 int find(int * array, int size, int target) {
 	int blockSize = sqrt(size);
-	for (int i = blockSize; i < size - 1; i += blockSize) {
-		if (target == array[i]) {
+	int blockLBound = 0;
+	// Find the possible block
+	for (int i = 0; i < size; i += blockSize) {
+		if (array[i] == target) {
 			return i;
+		} else if (array[i] > target) {
+			break;
 		}
-		if (target < array[i]) {
-			for (int j = i - blockSize; j < i; j++) {
-				if (array[j] == target) {
-					return j;
-				}
-			}
+		blockLBound = i;
+	}
+	// Do linear search on block
+	for (int i = blockLBound; i < blockLBound + blockSize; i++) {
+		if (array[i] == target) {
+			return i;
 		}
 	}
 	return -1;
@@ -22,7 +26,7 @@ int find(int * array, int size, int target) {
 int main() {
 	int array[] = {1, 2, 3, 4, 5, 6, 7};
 	int size = sizeof(array) / sizeof(array[0]);
-	int target = 5;
+	int target = 7;
 	int pos = find(array, size, target);
 	if (pos >= 0) {
 		printf("%d está na posição %d.\n", target, pos);
