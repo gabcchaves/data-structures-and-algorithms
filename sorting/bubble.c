12d34c138c48754b117d08a2591b858d155f7199
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-06-14 23:36:04 +0000

    Recursive bubble sorting.

--- sorting/bubble.c
+++ sorting/bubble.c
@@ -8,25 +8,48 @@ void swap(int *a, int *b) {
 	*b = temp;
 }
 
-void sort(int array[], int size) {
-	char sorted = 0;
-	while (!sorted) {
-		sorted = 1;
+void sortIterative(int array[], int size) {
+	char swapped;
+	do {
+		swapped = 0;
 		for (int i = 0; i < size-1; i++) {
 			if (array[i] > array[i+1]) {
 				swap(&array[i], &array[i+1]);
-				sorted = 0;
+				swapped = 1;
 			}
 		}
+	} while (swapped);
+}
+
+void sortRecursive(int array[], int size) {
+	if (size == 1) return;
+
+	char swapped = 0;
+	for (int i = 0; i < size; i++) {
+		if (array[i] > array[i+1]) {
+			swap(&array[i], &array[i+1]);
+			swapped = 1;
+		}
 	}
+	if (!swapped) return;
+
+	sortRecursive(array, size - 1);
 }
 
 int main() {
 	int array[] = {2, 1, 4, 5, 3, 6, 7};
 	int size = sizeof(array) / sizeof(array[0]);
+
 	printArray(array, size);
-	sort(array, size);
+
+	puts("Iterativo:");
+	sortIterative(array, size);
 	printArray(array, size);
+
+	puts("Recursivo:");
+	sortRecursive(array, size);
+	printArray(array, size);
+
 	return 0;
 }
 
