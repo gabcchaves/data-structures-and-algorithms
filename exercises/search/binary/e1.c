9f8141ca8c36c0e3518f84f1f8321bc132919ebe
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-06-14 19:58:16 +0000

    Finished both recursive and iterative implementations of binary search.

--- exercises/search/binary/e1.c
+++ exercises/search/binary/e1.c
@@ -5,13 +5,28 @@
 #include "../../../util.h"
 
 // Recursive implementation
-int find(int array[], int start, int end, int key) {
+int findRecursive(int array[], int start, int end, int key) {
 	if (start <= end) {
 		int mid = start + (end - start) / 2;
 		if (array[mid] == key) return mid;
-		if (array[mid] > key) find(array, start, mid - 1, key);
-		if (array[mid] < key) find(array, mid + 1, end, key);
+		if (array[mid] > key) findRecursive(array, start, mid - 1, key);
+		if (array[mid] < key) findRecursive(array, mid + 1, end, key);
 	}
+	return -1;
+}
+
+// Iterative
+int findIterative(int array[], int size, int key) {
+	int start = 0, end = size - 1, mid;
+	do {
+		mid = start + (end - start) / 2;
+		if (array[mid] == key) return mid;
+		if (array[mid] > key) {
+			end = mid - 1;
+		} else if (array[mid] < key) {
+			start = mid + 1;
+		}
+	} while (start <= end);
 	return -1;
 }
 
@@ -19,8 +34,15 @@ int main() {
 	int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 	int size = sizeof(array) / sizeof(array[0]);
 
-	int i = find(array, 0, size - 1, 5);
 	printArray(array, size);
+
+	puts("Recursivo:");
+	int i = findRecursive(array, 0, size - 1, 5);
 	printf("%d\n", i);
+
+	puts("Iterativo:");
+	i = findIterative(array, size, 5);
+	printf("%d\n", i);
+
 	return 0;
 }
