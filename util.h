97b21dac61904a6e8d858de3b4a76df97548c1f7
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-06-14 19:46:53 +0000

    Fill array with random elements, done; Fill array with sorted random elements, to continued.

--- util.h
+++ util.h
@@ -1,11 +1,29 @@
 #ifndef util
 #define util
 
+#include <time.h>
+#include <stdlib.h>
+
 void printArray(int array[], int size) {
 	printf("[ ");
 	for (int i = 0; i < size; i++)
 		printf("%d ", array[i]);
 	printf("]\n");
+}
+
+// Fill array with random elements (unsorted)
+void unsortedFill(int array[], int size) {
+	srand(time(NULL)); // Current time as seed for random generator
+	for (int i = 0; i < size; i++)
+		array[i] = rand() % 100;
+}
+
+// Fill array with random element (sorted)
+void sortedFill(int array[], int size) {
+	srand(time(NULL)); // Current time as seed for random generator
+	for (int i = 0; i < size; i++)
+		array[i] = rand() % 100;
+	// TODO
 }
 
 #endif
