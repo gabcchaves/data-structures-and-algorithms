4c8decc7e9e7aeb4c185df8453c29d9a0e0c543f
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-06-15 20:34:33 +0000

    Merge sorting algorithm.

--- sorting/merge.c
+++ sorting/merge.c
@@ -1,62 +1,60 @@
 /* Merge sorting algorithm. */
 #include <stdio.h>
+#include <time.h>
 #include "../util.h"
 
 void merge(int array[], int left, int mid, int right) {
-	/* Compute lengths of temporary arrays */
 	int s1 = mid - left + 1;
 	int s2 = right - mid;
 
-	/* Create temporary arrays */
-	int a1[s1], a2[s2];
+	int l1[s1],  l2[s2];
 
-	/* Copy data to temporary arrays */
+
 	for (int i = 0; i < s1; i++)
-		a1[i] = array[left + i];
+		l1[i] = array[left + i];
 	for (int i = 0; i < s2; i++)
-		a2[i] = array[mid + 1 + i];
+		l2[i] = array[mid + 1 + i];
 
-	/* Merge arrays */
-	int a1I = 0, a2I = 0, a3I = left;
-	while (a1I < s1 && a2I < s2) {
-		if (a1[a1I] <= a2[a2I]) {
-			array[a3I] = a1[a1I];
-			a1I++;
+	int i = 0, j = 0, k = 0;
+	while (i < s1 && j < s2) {
+		if (l1[i] <= l2[j]) {
+			array[k] = l1[i];
+			i++;
 		} else {
-			array[a3I] = a2[a2I];
-			a2I++;
+			array[k] = l2[j];
+			j++;
 		}
-		a3I++;
+		k++;
 	}
 
-	/* Copy remaining elements to array */
-	while (a1I < s1) {
-		array[a3I] = a2[a1I];
-		a1I++;
-		a3I++;
+	while (i < s1) {
+		array[k] = l1[i];
+		i++;
+		k++;
 	}
-	while (a2I < s2) {
-		array[a3I] = a2[a2I];
-		a2I++;
-		a3I++;
+
+	while (j < s2) {
+		array[k] = l2[j];
+		j++;
+		k++;
 	}
 }
 
 void sort(int array[], int left, int right) {
-	if (left >= right) return;
+	if (left < right) {
+		int mid = left + (right - left) / 2;
+		
+		sort(array, left, mid);
+		sort(array, mid + 1, right);
 
-	int mid = (left + right) / 2;
-	sort(array, left, mid);
-	sort(array, mid+1, right);
-	merge(array, left, mid, right);
+		merge(array, left, mid, right);
+	}
 }
 
 int main() {
-	int size = 5;
-	int array[size];
-	unsortedFill(array, size);
-	printArray(array, size);
-	sort(array, 0, size-1);
-	printArray(array, size);
+	int array[] = {3, 1, 2};
+	printArray(array, 3);
+	sort(array, 0, 2);
+	printArray(array, 3);
 	return 0;
 }
