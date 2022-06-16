a6bd7f0607a2f3dff9bced9e3d243c62c5e83534
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-06-16 14:30:54 -0300

    Sort a randomly generated array.

---
+++ exercises/sorting/selection/e4.c
@@ -0,0 +1,33 @@
+/* Sort a randomly generated array. */
+#include <stdio.h>
+#include <time.h>
+#include "../../../util.h"
+
+void swap(int *a, int *b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void sort(int array[], int size) {
+	for (int i = 0; i < size; i++) {
+		int least = i;
+		for (int j = i+1; j < size; j++) {
+			if (array[least] > array[j])
+				least = j;
+		}
+		swap(&array[i], &array[least]);
+	}
+}
+
+int main() {
+	srand(time(NULL));
+	unsigned int size = rand() % 4 + 2;
+	int array[size];
+	unsortedFill(array, size);
+	printArray(array, size);
+	sort(array, size);
+	printArray(array, size);
+	return 0;
+}
+
