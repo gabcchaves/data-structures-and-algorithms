c7f2cde9c182a26f351c1a40e5bf1ee8800fa862
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-07-03 12:36:13 -0300

    Queue implementation using array.

---
+++ exercises/lists/queues/e2.c
@@ -0,0 +1,68 @@
+/* Queue implementation with array. */
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Queue {
+	unsigned front, rear, size, capacity;
+	int *array;
+} Queue;
+
+Queue* initQueue(unsigned capacity) {
+	Queue* queue = (Queue*) malloc(sizeof(Queue));
+	queue->capacity = capacity;
+	queue->front = 0;
+	queue->size = 0;
+	queue->rear = capacity - 1;
+	queue->array = (int*) malloc(queue->capacity * sizeof(int));
+}
+
+char isFull(Queue* queue) {
+	return (queue->size == queue->capacity);
+}
+
+char isEmpty(Queue* queue) {
+	return (queue->size == 0);
+}
+
+void enqueue(Queue* queue, int item) {
+	if (isFull(queue)) return;
+	queue->rear = (queue->rear + 1) % queue->capacity;
+	queue->array[queue->rear] = item;
+	queue->size++;
+}
+
+int dequeue(Queue* queue) {
+	if (isEmpty(queue)) return -1;
+	int item = queue->array[queue->front];
+	queue->front = (queue->front + 1) % queue->capacity;
+	queue->size--;
+	printf("%d removido da fila.\n", item);
+	return item;
+}
+
+void printQueue(Queue* queue) {
+	printf("[ ");
+	for (unsigned i = 0; i < queue->size; i++)
+		printf("%d ", queue->array[i]);
+	puts("]");
+}
+
+int main() {
+	Queue* queue = initQueue(3);
+
+	printQueue(queue);
+	enqueue(queue, 10);
+	printQueue(queue);
+	enqueue(queue, 20);
+	printQueue(queue);
+	enqueue(queue, 30);
+	printQueue(queue);
+
+	dequeue(queue);
+	dequeue(queue);
+	dequeue(queue);
+
+	printQueue(queue);
+
+	return 0;
+}
