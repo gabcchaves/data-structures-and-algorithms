05b4aa38017f2a894e0a75fa78effbaa5a470738
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-10-04 18:00:34 -0300

    Current state. Segmentation fault.

--- btree_linked_list.c
+++ btree_linked_list.c
@@ -1,6 +1,10 @@
 /* Program to create and manage binary trees with linked list. */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "util.h"
 
 typedef struct BTreeNode {
 	int data;
@@ -8,18 +12,40 @@ typedef struct BTreeNode {
 	struct BTreeNode *right;
 } BTreeNode;
 
+// Create binary tree
+void create_btree(BTreeNode root_node) {
+}
+
+// Read integer (treated)
+void read_number(ResultGeneric target) {
+	char c;
+	char *str_num;
+	size_t str_index = -1;
+	while (scanf("%c", c) && c != '\n') {
+		if (!isdigit(c)) {
+			target.status = 0;
+		}
+		str_index++;
+		str_num = realloc(str_num, str_index);
+		str_num[str_index] = c;
+	}
+	parse(str_num, target);
+}
+
 int main() {
-	BTreeNode node0;
-	BTreeNode node1;
-	BTreeNode node2;
-
-	node0.data = 10;
-	node1.data = 20;
-	node2.data = 30;
-
-	node0.left = &node1;
-	node0.right = &node2;
-
-	printf("\t[%d]\n[%d]\t\t[%d]\n", node0.data, node0.left->data, node0.right->data);
+	ResultGeneric input_number;
+	read_number(input_number);
+	printf("%d", input_number.status);
+	if (input_number.status) {
+		if (input_number.is_float) {
+			float n = input_number.value_float.value;
+			printf("%.2f", n);
+		} else {
+			int n = input_number.value_int.value;
+			printf("%d", n);
+		}
+	} else {
+		printf("Invalid number entered.\n");
+	}
 	return 0;
 }
