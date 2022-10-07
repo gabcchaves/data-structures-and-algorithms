4b104d33c9a8a7594550da08f3a68b21e8a0178c
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-10-06 21:02:30 -0300

    Product insertion.

--- exercises/productRegistration.c
+++ exercises/productRegistration.c
@@ -16,56 +16,67 @@ typedef struct Product {
 typedef struct ProductList {
 	Product *products;
 	unsigned int num_products;
-	unsigned int index_first;
 	unsigned int index_last;
 } ProductList;
 
-// Proc. adicionar produto
-void add_product(ProductList product_list, char *description, double price, unsigned int quantity) {
-	if (product_list.num_products == 0) {
-		Product new_product = { 1, description, price, quantity };
-		product_list.products = malloc(sizeof(Product) * 1);
-		product_list.products[0] = new_product;
-		product_list.num_products++;
-		product_list.index_first = 0;
-		product_list.index_last = 0;
+// Proc. adicionar produto à lista de produtos
+void add_product(ProductList *product_list, Product product) {
+	if (product_list->num_products == 0) {
+		product.id = product_list->num_products + 1;
+		product_list->products = malloc(sizeof(Product) * (product_list->num_products + 1));
+		product_list->products[0] = product;
+		product_list->num_products++;
+		product_list->index_last = 0;
 	} else {
-		Product new_product = {
-			product_list.num_products,
-			description,
-			price,
-			quantity
-		};
-		realloc(product_list.products, sizeof(product_list.products) + sizeof(Product));
-		product_list.products[product_list.index_last] = new_product;
-		product_list.index_last++;
-		product_list.num_products++;
+		product.id = product_list->num_products + 1;
+		realloc(product_list->products, sizeof(Product) * (product_list->num_products + 1));
+		product_list->index_last++;
+		product_list->products[product_list->index_last] = product;
+		product_list->num_products++;
 	}
 }
 
-// Proc. exibir lista completa de produtos
-void print_all_products(ProductList product_list) {
+// Proc. exibir todos os produtos da lista de produtos
+void print_products(ProductList *product_list) {
+	unsigned int length = product_list->num_products;
 	printf("LISTA DE PRODUTOS\n");
-	Product *p = product_list.products;
-	size_t length = sizeof(p) / sizeof(Product);
 	for (unsigned int i = 0; i < length; i++) {
-		printf("%u", p[i].id);
-		printf("%s", p[i].description);
-		printf("%.2f", p[i].price);
-		printf("%u\n", p[i].quantity);
+		printf("%u - ", product_list->products[i].id);
+		printf("%s\t\t", product_list->products[i].description);
+		printf("%.2f\t ", product_list->products[i].price);
+		printf("%u\n", product_list->products[i].quantity);
 	}
+}
+
+// Func. inicializa lista de produtos
+ProductList init_list() {
+	ProductList product_list = {
+		NULL,
+		0,
+		NULL
+	};
+	return product_list;
+}
+
+// Func. criar produto
+Product create_product(char * description, double price, unsigned int quantity) {
+	Product product = {
+		NULL,
+		description,
+		price,
+		quantity
+	};
+	return product;
 }
 
 // Modulo principal
 int main() {
-	ProductList products;
-	add_product(products, "Pizza", 5.00, 2);
-	add_product(products, "Pão", 2.00, 2);
-
-	print_all_products(products);
-	
-	free(products.products);
-
+	ProductList products = init_list();
+	add_product(&products, create_product("Pão", 0.1, 2));
+	add_product(&products, create_product("Leite", 0.1, 2));
+	add_product(&products, create_product("Arroz", 0.1, 2));
+	add_product(&products, create_product("Feijão", 0.1, 2));
+	print_products(&products);
 	return 0;
 }
 
