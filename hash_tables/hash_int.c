fbd9a29dc4363507447d454bbb6a8416f1bd7c1a
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-10-21 14:25:02 -0300

    Working linked list.

--- hash_tables/hash_int.c
+++ hash_tables/hash_int.c
@@ -2,44 +2,82 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "../lib/util.h"
 
-// Initialize hash table.
-unsigned short* newHashTable(const size_t HT_LENGTH) {
-	unsigned short *array = (unsigned short*) malloc(HT_LENGTH * sizeof(unsigned short));
-	memset(array, 0, HT_LENGTH);
-	return array;
+// List.
+typedef struct ListNode ListNode;
+typedef struct List List;
+
+struct ListNode {
+	unsigned int data;
+	ListNode *next;
+};
+
+struct List {
+	ListNode *first;
+	ListNode *last;
+};
+
+// Create a new list.
+List* newList(size_t length) {
+	List *list = (List*) malloc(length * sizeof(NULL));
+	list->first = NULL;
+	list->last = NULL;
+	return list;
 }
 
-// Hash function.
-unsigned short get(unsigned int key, unsigned int table_length) {
-	return key % table_length;
-}
-
-// Add element to the hash table.
-char insert(unsigned short hash_table[], const size_t HT_LENGTH, unsigned short key) {
-	unsigned short value = get(key, HT_LENGTH);
-	if (value < HT_LENGTH) {
-		
+// Add element to list.
+void addToList(List *list, unsigned int data) {
+	ListNode *new_node = (ListNode*) malloc(sizeof(ListNode));
+	new_node->data = data;
+	new_node->next = NULL;
+	
+	if (list->first != NULL) {
+		list->last->next = new_node;
+		list->last = new_node;
+	} else {
+		list->first = new_node;
+		list->last = new_node;
 	}
 }
 
-int main() {
-	const size_t HT_LENGTH = 10;
-	unsigned short *hash_table = newHashTable(HT_LENGTH);
-
-	printUshortArray(hash_table, HT_LENGTH);
-	printf("\n");
-
-	//unsigned short hash_values[] = {
-	//};
-
-	//size_t length_hash_values = sizeof(hash_values) / sizeof(hash_values[0]);
-
-	//printUshortArray(hash_values, length_hash_values);
-	//printf("\n");
-
-	//printf("%u\n", hash_table[hash_values[0]]);
-	return 0;
+// Remove element from the list.
+void rm(List *list, unsigned int data) {
+	ListNode *curr = list->first;
+	ListNode *next = curr->next;
+	while (next != NULL) {
+		if (next->data == data) {
+			curr->next = next->next;
+		}
+		curr = curr->next;
+		next = curr->next;
+	}
 }
 
+// Print list.
+void printList(List *list) {
+	ListNode *curr = list->first;
+	while (curr != NULL) {
+		printf("%u ", curr->data);
+		curr = curr->next;
+	}
+	printf("\b");
+}
+
+int main() {
+	size_t length = 10;
+	List *list = newList(length);
+	
+	addToList(list, 10);
+	addToList(list, 20);
+	addToList(list, 30);
+	
+	printList(list);
+	printf("\n");
+	
+	rm(list, 20);
+	
+	printList(list);
+	printf("\n");
+
+	return 0;
+}
